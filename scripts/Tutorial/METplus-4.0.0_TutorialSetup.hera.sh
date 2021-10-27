d4b6b6f8297ad154f359fe1bbc799c06feea496d
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2021-10-27 15:19:49 -0600

    updated path to sample data

--- scripts/Tutorial/METplus-4.0.0_TutorialSetup.hera.sh
+++ scripts/Tutorial/METplus-4.0.0_TutorialSetup.hera.sh
@@ -6,4 +6,4 @@ module load metplus/4.0.0
 export METPLUS_TUTORIAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export METPLUS_BUILD_BASE=/contrib/METplus/METplus-4.0.0
 export MET_BUILD_BASE=/contrib/met/10.0.0
-export METPLUS_DATA=/scratch1/BMC/dtc/Julie.Prestopnik/METplus/METplus-4.0_sample_data
+export METPLUS_DATA=/scratch1/BMC/dtc/METplus/METplus-4.0_sample_data
